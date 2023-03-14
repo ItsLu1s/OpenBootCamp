f273c5bfb83cd2e3fdd9044e816fa9326321e20b
ItsLu1s <125322085+ItsLu1s@users.noreply.github.com>
2023-03-13 19:35:33 -0500

    Update Ejercicio5-Python.py

--- Ejercicio5-Python.py
+++ Ejercicio5-Python.py
@@ -1,8 +1,10 @@
-def Bisiesto(year):
-    if year % 4 == 0:
+def Bisiesto():
+    year = int(input("Inserte el año aquí: "))
+    
+    if year % 4 == 0 and year % 400:
         return str(year) + " es bisiesto"
     else:
         return str(year) + " no es bisiesto"
 
 
-print(Bisiesto(2023))
+print(Bisiesto())
